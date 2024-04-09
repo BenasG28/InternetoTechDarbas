1d9d8bafc344ab0b6d178486e09a35de33ee0078
BenasG28 <114530547+BenasG28@users.noreply.github.com>
2024-04-09 18:19:18 +0300

    added template and other configs

---
+++ front-end/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Navigation */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        {/* Navbar content */}
+      </nav>
+
+      {/* Header */}
+      <header className="bg-dark py-5">
+        {/* Header content */}
+      </header>
+
+      {/* Section */}
+      <section className="py-5">
+        {/* Section content */}
+      </section>
+
+      {/* Footer */}
+      <footer className="py-5 bg-dark">
+        {/* Footer content */}
+      </footer>
+    </div>
+  );
+}
+
+export default App;
