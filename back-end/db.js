37a5436c572f467d3faf7a6635c350ebd4282801
BenasG28 <114530547+BenasG28@users.noreply.github.com>
2024-04-10 17:56:45 +0300

    added user creation and linked it with registration

--- back-end/db.js
+++ back-end/db.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
-
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
 // Open a connection to the SQLite database file
 const db = new sqlite3.Database('gklDatabase.db', (err) => {
     if (err) {
@@ -14,7 +15,8 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE,
     password TEXT,
-    cardNumber TEXT
+    cardNumber TEXT,
+    token TEXT
 )`, (err) => {
     if (err) {
         console.error('Error creating users table:', err.message);
@@ -22,10 +24,43 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
         console.log('Users table already exists or was created successfully.');    }
 });
 
-db.close((err) => {
-    if (err) {
-        console.error('Error closing database:', err.message);
-    } else {
-        console.log('Database connection closed.');
-    }
-});
+module.exports.createUserWithToken = (username, password, cardNumber, token, callback) => {
+     // Generate a salt and hash for the password
+     bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+            return callback(err);
+        }
+        const sql = `INSERT INTO users (username, password, cardNumber, token) VALUES (?, ?, ?, ?)`;
+        db.run(sql, [username, hashedPassword, cardNumber, token], function (err) {
+            if (err) {
+                callback(err);
+            } else {
+                // Return the ID of the newly inserted user along with the token
+                callback(null, { id: this.lastID, token });
+            }
+        });
+    });
+  };
+  module.exports.findUserByUsername = (username, callback) => {
+    const sql = `SELECT * FROM users WHERE username = ?`;
+    db.get(sql, [username], (err, row) => {
+        if (err) {
+            callback(err);
+        } else {
+            // If a user with the given username exists, return the user data
+            // Otherwise, return null to indicate that the user does not exist
+            callback(null, row);
+        }
+    });
+};  
+
+// Close the database connection
+module.exports.closeDatabase = () => {
+    db.close((err) => {
+        if (err) {
+            console.error('Error closing database:', err.message);
+        } else {
+            console.log('Database connection closed.');
+        }
+    });
+};
