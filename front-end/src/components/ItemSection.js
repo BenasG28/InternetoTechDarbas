7a8f2929982b55628573135d33e1a7db9a2cf3d8
SLUSNYS <earlyaccess1234@gmail.com>
2024-04-11 01:22:55 +0300

    dar komentarai :((((((((((((((((()))))))))))))))))

--- front-end/src/components/ItemSection.js
+++ front-end/src/components/ItemSection.js
@@ -1,38 +1,3 @@
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-
-// const ItemSection = ({ imageSrc, productName, price, onSale, starReviews }) => {
-  // return (
-  //   <div>
-  //     <section className="py-5">
-  //       <div className="container px-4 px-lg-5 my-5">
-  //         <div className="row gx-4 gx-lg-5 align-items-center">
-  //           <div className="col-md-6"><img className="card-img-top mb-5 mb-md-0" src={imageSrc} alt="..." /></div>
-  //           <div className="col-md-6">
-  //             <div className="small mb-1">SKU: {productName}</div>
-  //             <h1 className="display-5 fw-bolder">{productName}</h1>
-  //             <div className="fs-5 mb-5">
-  //               {onSale && <span className="text-decoration-line-through">{price}</span>}
-  //               <span>{price}</span>
-  //             </div>
-  //             <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
-  //             <div className="d-flex">
-  //               <input className="form-control text-center me-3" id="inputQuantity" type="number" defaultValue="1" style={{ maxWidth: "3rem" }} />
-  //               <button className="btn btn-outline-dark flex-shrink-0" type="button">
-  //                 <i className="bi-cart-fill me-1"></i>
-  //                 Add to cart
-  //               </button>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </section>
-  //   </div>
-//   );
-// }
-
-// export default ItemSection;
-
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
